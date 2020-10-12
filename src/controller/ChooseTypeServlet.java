eb541d8298854929170679f379d0f2c42f25a062
chasevanblair <crvanblair@dmacc.edu>
2020-10-12 16:39:34 -0500

    index created
    first choice servlet created and index basics

---
+++ src/controller/ChooseTypeServlet.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+//this is where the user is either directed to a camper/hiker page
+
+
+
+/**
+ * Servlet implementation class ChooseTypeServlet
+ */
+@WebServlet("/ChooseTypeServlet")
+public class ChooseTypeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChooseTypeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String choice = request.getParameter("choice");
+		
+		if (choice.equals("camper")) {
+			//camper.jsp not yet created
+			getServletContext().getRequestDispatcher("/camper.jsp").forward(request, response);
+		}
+		else if(choice.equals("hiker")) {
+			getServletContext().getRequestDispatcher("/hiker.jsp").forward(request, response);
+		}
+	}
+
+}
